ad2a39aec6735dbb5141096182e5f0239d2c026b
yuvenious <37578231+yuvenious@users.noreply.github.com>
2018-05-31 18:30:23 +0200

    MORDM_Step2_finished
    assignment for week 6 Multi-Objectives Robust Decision Making

---
+++ week_6-7/lake.py
@@ -0,0 +1,50 @@
+import numpy as np
+import math
+from scipy.optimize import brentq
+
+def lake_problem(
+    b=0.42,          # decay rate for P in lake (0.42 = irreversible)
+    q=2.0,           # recycling exponent
+    mean=0.02,       # mean of natural inflows
+    stdev=0.001,     # future utility discount rate
+    delta=0.98,      # standard deviation of natural inflows
+    alpha=0.4,       # utility from pollution
+    nsamples=100,    # Monte Carlo sampling of natural inflows
+        **kwargs):
+    try:
+        decisions = [kwargs[str(i)] for i in range(100)]
+    except KeyError:
+        decisions = [0, ] * 100
+
+    Pcrit = brentq(lambda x: x**q/(1+x**q) - b*x, 0.01, 1.5)
+    nvars = len(decisions)
+    X = np.zeros((nvars,))
+    average_daily_P = np.zeros((nvars,))
+    decisions = np.array(decisions)
+    reliability = 0.0
+
+    for _ in range(nsamples):
+        X[0] = 0.0
+
+        natural_inflows = np.random.lognormal(
+            math.log(mean**2 / math.sqrt(stdev**2 + mean**2)),
+            math.sqrt(math.log(1.0 + stdev**2 / mean**2)),
+            size=nvars)
+
+        for t in range(1, nvars):
+            X[t] = (1-b)*X[t-1] + X[t-1]**q/(1+X[t-1]**q) + \
+                decisions[t-1] + natural_inflows[t-1]
+            average_daily_P[t] += X[t]/float(nsamples)
+
+        reliability += np.sum(X < Pcrit)/float(nsamples*nvars)
+
+    max_P = np.max(average_daily_P)
+    utility = np.sum(alpha*decisions*np.power(delta, np.arange(nvars)))
+    inertia = np.sum(np.absolute(np.diff(decisions)) < 0.02)/float(nvars-1)
+
+    return max_P, utility, inertia, reliability
+
+def process_p(values):
+    values = np.asarray(values)
+    values = np.mean(values, axis=0)
+    return np.max(values)
--- week_6-7/lakemodel_function.py
+++ week_6-7/lakemodel_function.py
@@ -1,50 +1,142 @@
-'''
-Created on May 2, 2017
-
-@author: jhkwakkel
-'''
-from __future__ import division
-
 import math
+import ast
 import numpy as np
+import pandas as pd
 
 from scipy.optimize import brentq
 
-def lake_problem(
-         b = 0.42,          # decay rate for P in lake (0.42 = irreversible)
-         q = 2.0,           # recycling exponent
-         mean = 0.02,       # mean of natural inflows
-         stdev = 0.0017,     # future utility discount rate
-         delta = 0.98,      # standard deviation of natural inflows
-         alpha = 0.4,       # utility from pollution
-         nsamples = 100,    # Monte Carlo sampling of natural inflows
-         **kwargs):   
-    decisions = [kwargs[str(i)] for i in range(nsamples)]
-    
+
+def get_antropogenic_release(xt, c1, c2, r1, r2, w1):
+    '''
+
+    Parameters
+    ----------
+    xt : float
+         polution in lake at time t
+    c1 : float
+         center rbf 1
+    c2 : float
+         center rbf 2
+    r1 : float
+         ratius rbf 1
+    r2 : float
+         ratius rbf 2
+    w1 : float
+         weight of rbf 1
+
+    Returns
+    -------
+    float
+
+    note:: w2 = 1 - w1
+
+    '''
+
+    rule = w1*(abs(xt-c1)/r1)**3+(1-w1)*(abs(xt-c2)/r2)**3
+    at1 = max(rule, 0.01)
+    at = min(at1, 0.1)
+
+    return at
+
+
+def lake_model(b=0.42, q=2.0, mean=0.02,
+               stdev=0.001, delta=0.98, alpha=0.4,
+               nsamples=100, myears=100, c1=0.25,
+               c2=0.25, r1=0.5, r2=0.5,
+               w1=0.5, seed=None):
+    '''runs the lake model for nsamples stochastic realisation using
+    specified random seed.
+
+    Parameters
+    ----------
+    b : float
+        decay rate for P in lake (0.42 = irreversible)
+    q : float
+        recycling exponent
+    mean : float
+            mean of natural inflows
+    stdev : float
+            standard deviation of natural inflows
+    delta : float
+            future utility discount rate
+    alpha : float
+            utility from pollution
+    nsamples : int, optional
+    myears : int, optional
+    c1 : float
+    c2 : float
+    r1 : float
+    r2 : float
+    w1 : float
+    seed : int, optional
+           seed for the random number generator
+
+    Returns
+    -------
+    tuple
+
+    '''
+    np.random.seed(seed)
     Pcrit = brentq(lambda x: x**q/(1+x**q) - b*x, 0.01, 1.5)
-    nvars = len(decisions)
-    X = np.zeros((nvars,))
-    average_daily_P = np.zeros((nvars,))
-    decisions = np.array(decisions)
+
+    X = np.zeros((myears,))
+    average_daily_P = np.zeros((myears,))
     reliability = 0.0
+    inertia = 0
+    utility = 0
 
     for _ in range(nsamples):
         X[0] = 0.0
-        
-        natural_inflows = np.random.lognormal(
-                math.log(mean**2 / math.sqrt(stdev**2 + mean**2)),
-                math.sqrt(math.log(1.0 + stdev**2 / mean**2)),
-                size = nvars)
-        
-        for t in range(1,nvars):
-            X[t] = (1-b)*X[t-1] + X[t-1]**q/(1+X[t-1]**q) + decisions[t-1] +\
-                    natural_inflows[t-1]
-            average_daily_P[t] += X[t]/float(nsamples)
-    
-        reliability += np.sum(X < Pcrit)/float(nsamples*nvars)
-      
-    max_P = np.max(average_daily_P)
-    utility = np.sum(alpha*decisions*np.power(delta,np.arange(nvars)))
-    inertia = np.sum(np.abs(np.diff(decisions)) > 0.02)/float(nvars-1)
+        decision = 0.1
 
+        decisions = np.zeros(myears,)
+        decisions[0] = decision
+
+        natural_inflows = np.random.lognormal(
+            math.log(mean**2 / math.sqrt(stdev**2 + mean**2)),
+            math.sqrt(math.log(1.0 + stdev**2 / mean**2)),
+            size=myears)
+
+        for t in range(1, myears):
+
+            # here we use the decision rule
+            decision = get_antropogenic_release(X[t-1], c1, c2, r1, r2, w1)
+            decisions[t] = decision
+
+            X[t] = (1-b)*X[t-1] + X[t-1]**q/(1+X[t-1]**q) + decision +\
+                natural_inflows[t-1]
+            average_daily_P[t] += X[t]/nsamples
+
+        reliability += np.sum(X < Pcrit)/(nsamples*myears)
+        inertia += np.sum(np.absolute(np.diff(decisions)
+                                      < 0.02)) / (nsamples*myears)
+        utility += np.sum(alpha*decisions*np.power(delta,
+                                                   np.arange(myears))) / nsamples
+    max_P = np.max(average_daily_P)
     return max_P, utility, inertia, reliability
+
+def make_df(results):
+    experiments, outcomes = results
+
+    # DATA of INPUTS
+    experiments = np.lib.recfunctions.drop_fields(base = experiments, drop_names = "c1	c2	r1	r2	w1".split("	"))
+    experiments = pd.DataFrame(experiments)
+
+    n_scenarios = experiments["scenario_id"].nunique()
+    n_policies = experiments["policy"].nunique()
+
+    # POLICIES (STRING representation into DICTIONARY)
+    policies = experiments["policy"].unique()
+    func = lambda x: ast.literal_eval(x)
+    vfunc = np.vectorize(func)
+    policies = vfunc(policies)
+    policies = pd.DataFrame(policies)
+
+    experiments["policy"] = np.repeat(range(n_policies), n_scenarios)
+
+    # DATA of OUTPUTS
+    outcomes = pd.DataFrame(outcomes)
+    outcomes["policy"] = experiments["policy"].values
+    outcomes.set_index("policy", inplace = True)
+
+    return experiments, outcomes, policies
